1ec42e6412801c2945dbf5a9b157f614956be531
teluguanimes60-ops <teluguanimes60@gmail.com>
2026-06-24 12:05:08 +0530

    Create help_text.py

---
+++ help_text.py
@@ -0,0 +1,23 @@
+HELP_TEXT = """
+📌 **How to Use Bot**
+
+1. Send a file
+2. Send new name
+3. Choose format
+4. Get renamed file
+
+🖼 Thumbnail:
+- Send photo to save thumbnail
+
+🎬 Supported:
+- Video
+- Audio
+- Documents
+
+⚡ Features:
+- Fast processing
+- Progress bar + speed + ETA
+- Custom thumbnail
+
+👨‍💻 Made by AniToon
+"""
