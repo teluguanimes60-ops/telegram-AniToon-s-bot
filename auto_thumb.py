4e83ce09efb4239548740979eec2c0f88913e90a
teluguanimes60-ops <teluguanimes60@gmail.com>
2026-06-24 17:52:20 +0530

    Create auto_thumb.py

---
+++ auto_thumb.py
@@ -0,0 +1,26 @@
+import os
+import subprocess
+
+def generate_thumbnail(video_path):
+    thumb_path = video_path + ".jpg"
+
+    try:
+        # take frame at 2nd second
+        cmd = [
+            "ffmpeg",
+            "-ss", "00:00:02",
+            "-i", video_path,
+            "-frames:v", "1",
+            "-q:v", "2",
+            thumb_path
+        ]
+
+        subprocess.run(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+        if os.path.exists(thumb_path):
+            return thumb_path
+        else:
+            return None
+
+    except:
+        return None
