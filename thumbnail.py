ab4d9c40caa20f08e868548eac09f0fc58a92e56
teluguanimes60-ops <teluguanimes60@gmail.com>
2026-06-24 14:48:46 +0530

    Update thumbnail.py

--- thumbnail.py
+++ thumbnail.py
@@ -3,7 +3,9 @@ import os
 THUMB_PATH = "thumb.jpg"
 
 def save_thumb(path):
-    os.replace(path, THUMB_PATH)
+    if os.path.exists(THUMB_PATH):
+        os.remove(THUMB_PATH)
+    os.rename(path, THUMB_PATH)
 
 def get_thumb():
     return THUMB_PATH if os.path.exists(THUMB_PATH) else None
